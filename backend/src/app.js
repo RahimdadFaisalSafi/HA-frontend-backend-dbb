13de6b0ffb54ea6ee329e232d043067af2553df5
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2025-01-23 20:29:55 +0100

    Complete login endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const db = require('../database/database')
+const bcrypt = require('bcrypt')
 
 const PORT = 3000;
 
@@ -10,16 +12,38 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    username = req.body.username
-    password = req.body.password
+    const username = req.body.username
+    let password = req.body.password
 
     if(!username || !password){
         return res.status(400).send("Missing required fields: username and/or password.")
     }
-    const hashedpassword = bcrypt.hashSync(password, 12)
-    const query = db.prepare("INSERT INTO users (username, password) VALUES(?, ?)")
-    const insert = query.run(username, password)
+    const hashedPassword = bcrypt.hashSync(password, 12)
+    password = hashedPassword
+    const query = db.db.prepare("INSERT INTO users (username, password) VALUES(?, ?)")
+    const insert = query.run(username, hashedPassword)
+    res.status(200).send("User added successfully");
+})
 
+app.post('/login', (req, res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!username || !password){
+        return res.status(400).send("Missing required fields: username and/or password.")
+    }
+
+    const query = db.db.prepare("SELECT * FROM users WHERE username = ?").get(username)
+    if(!query) {
+        return res.status(404).send("User Not Found")
+    }
+
+    const compare = bcrypt.compareSync(password, query.password)
+    if(compare) {
+        return res.status(200).send("Login Successfully")
+    } else {
+        res.status(400).send("Invalid Username || Password")
+    }
 
 
 
